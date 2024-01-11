cc27eff65fcc0247ab3edf80b823f331942ace8d
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-11 21:58:36 +0530

    cors removed Deployment Error fixed

--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -18,7 +18,6 @@ export const createServer = () => {
       cors({
         origin: process.env.CLIENT_URL, // Allow requests from CLIENT_URL
         methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-        allowedHeaders: "X-Requested-With, Content-Type, Authorization",
         credentials: true,
       })
     );
