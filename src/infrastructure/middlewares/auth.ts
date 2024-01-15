6196c3636a22ea906b1caab6fc3ec2f77cc982ff
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-15 12:36:26 +0530

    interceptor update

--- src/infrastructure/middlewares/auth.ts
+++ src/infrastructure/middlewares/auth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { OAuth2Client, TokenPayload } from "google-auth-library";
 import IGoogleAuthUser from "../../domain/entities/googleAuth";
 import UserRepository from "../repositories/userRepository";
-import jwt, { JwtPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
@@ -26,30 +26,28 @@ const auth = async (
   next: NextFunction
 ) => {
   try {
-    // const token = req.headers.authorization?.split(" ")[1];
-    // const googleToken = token?.length! > 1000;
+    const token = req.headers.authorization?.split(" ")[1];
+    const googleToken = token?.length! > 1000;
+    if (googleToken) {
+      const ticket = await client.verifyIdToken({
+        idToken: token!,
+        audience: process.env.GOOGLE_CLIENT_ID!,
+      });
 
-    // if (googleToken) {
-    //   const ticket = await client.verifyIdToken({
-    //     idToken: token!,
-    //     audience: process.env.GOOGLE_CLIENT_ID!,
-    //   });
+      const payload = ticket.getPayload();
+      const userPayload: TokenPayload = payload as TokenPayload;
 
-    //   const payload = ticket.getPayload();
-    //   const userPayload: TokenPayload = payload as TokenPayload;
-
-    //   req.user = {
-    //     _id: userPayload?.sub!,
-    //     firstName: userPayload?.given_name!,
-    //     lastName: userPayload?.family_name!,
-    //     email: userPayload?.email!,
-    //     profileImage: userPayload?.picture!,
-    //   };
-    // } else {
+      req.user = {
+        _id: userPayload?.sub!,
+        firstName: userPayload?.given_name!,
+        lastName: userPayload?.family_name!,
+        email: userPayload?.email!,
+        profileImage: userPayload?.picture!,
+      };
+    } else {
       // To do: verify our custom jwt token
       let token;
       token = req.cookies.userJWT;
-      console.log("tokeen",token);
       if (token) {
         const decoded = jwt.decode(token) as JwtPayload;
         if (decoded.role === "user") {
@@ -75,7 +73,7 @@ const auth = async (
           result: { success: false, message: "Unauthorized Access" },
         });
       }
-    // }
+    }
   } catch (error) {
     console.error(error);
     res.status(401).json({
