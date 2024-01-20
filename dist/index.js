4c96702eb3b7037abbbf484f2d33fc0300aa6c6d
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-21 00:23:44 +0530

    index.ts undo updated

--- dist/index.js
+++ dist/index.js
@@ -6,17 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./infrastructure/config/app");
 const db_1 = __importDefault(require("./infrastructure/config/db"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, app_1.createServer)();
 const port = process.env.PORT || 6000;
-app.use((0, cors_1.default)({
-    origin: process.env.CLIENT_URL,
-    // methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-    // allowedHeaders: "X-Requested-With, Content-Type, Authorization",
-    credentials: true,
-}));
-app.options("*", (0, cors_1.default)());
 (0, db_1.default)().then(() => {
     app === null || app === void 0 ? void 0 : app.listen(port, () => console.log(`Example app listening on port ${port}`));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import { createServer } from "./infrastructure/config/app";
 import connectDB from "./infrastructure/config/db";
 import dotenv from "dotenv";
-import cors from "cors";
-
 dotenv.config();
-const app:any = createServer();
+const app = createServer();
 const port = process.env.PORT || 6000;
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL, // Allow requests from CLIENT_URL
-    // methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-    // allowedHeaders: "X-Requested-With, Content-Type, Authorization",
-    credentials: true,
-  })
-);
-app.options("*", cors());
 
 connectDB().then(() => {
   app?.listen(port, () => console.log(`Example app listening on port ${port}`));
