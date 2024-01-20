84db39c153ecbbbc817e44580ad5b66354dba05d
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-21 00:20:13 +0530

    index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { createServer } from "./infrastructure/config/app";
 import connectDB from "./infrastructure/config/db";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
-const app = createServer();
+const app:any = createServer();
 const port = process.env.PORT || 6000;
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, // Allow requests from CLIENT_URL
+    // methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    // allowedHeaders: "X-Requested-With, Content-Type, Authorization",
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 connectDB().then(() => {
   app?.listen(port, () => console.log(`Example app listening on port ${port}`));
