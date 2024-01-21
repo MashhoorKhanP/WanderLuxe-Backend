28e199184f87525f935fcb501eb5f5d9453cc604
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-21 12:22:18 +0530

    app.ts updated cors area

--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -16,9 +16,7 @@ export const createServer = () => {
 
     app.use(
       cors({
-        origin: process.env.CLIENT_URL, // Allow requests from CLIENT_URL
-        // methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-        // allowedHeaders: "X-Requested-With, Content-Type, Authorization",
+        origin: process.env.CLIENT_URL,
         credentials: true,
       })
     );
