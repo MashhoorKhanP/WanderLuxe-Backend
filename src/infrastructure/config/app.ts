03e47ad05c4e43ad1d73fca8a90bf5df727fe591
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-02-20 10:03:35 +0530

    cors issue for checking

--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import http from "http";
 import userRoute from "../routes/userRoute";
 import adminRoute from "../routes/adminRoute";
@@ -20,7 +20,7 @@ export const createServer = () => {
     //     credentials: true,
     //   }) 
     // );
-    // app.options("*", cors());
+    app.options("*", cors());
 
     app.use(express.json({ limit: "10mb" }));
     app.use(express.urlencoded({ extended: true }));
